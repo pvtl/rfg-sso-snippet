a4aa25ed1553d9a53154d85e8feb9dbd3a62e8d1
Sheldon <sheldon.b@pivotalagency.com.au>
2019-04-24 08:53:36 +1000

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# RFG SSO Snippet
+
+WordPress plugin that Includes the RFG Single-Sign-On (SSO) code snippet on the wp-login page programmatically.
+
+## Installation into a Bedrock site
+
+#### Install
+
+```
+# 1. Get it ready (to use a repo outside of packagist)
+composer config repositories.rfg-sso-snippet git https://bitbucket.org/pvtl/rfg-sso-snippet
+
+# 2. Install the Plugin
+composer require pvtl/rfg-sso-snippet
+```
+
+#### Activate / Configure
+
+- Activate the plugin
---
+++ rfg-sso-snippet.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: RFG SSO Snippet
+Plugin URI: https://www.pivotalagency.com.au/
+Description: Inserts RFG Single-Sign-On (SSO) snippet into wp-login.php programmatically.
+Version: 1.0.0
+Author: Pivotal Agency
+Author URI: https://www.pivotalagency.com.au/
+Text Domain: rfg-sso-snippet
+License: GPL3+
+*/
+
+// Only load class if it hasn't already been loaded
+if ( !class_exists( 'RfgSsoSnippet' ) ) {
+
+	// RFG SSO Code Snippet - All the magic happens here!
+	class RfgSsoSnippet {
+
+		static $didInit = false;
+
+		public function __construct() {
+			if (!self::$didInit) {
+				$this->init();
+				self::$didInit = true;
+			}
+		}
+
+		private function init() {
+			// Internationalization
+			load_plugin_textdomain( 'rfg-sso-snippet', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+			// Add Actions
+			add_action( 'login_enqueue_scripts', array( $this, 'admin_enqueue_scripts_styles' ) ); // Enqueue styles on relevant page
+			add_action( 'login_form', array( $this, 'output_snippet' ) ); // Code output
+		}
+
+
+		/**
+		 * Adds JS/CSS to admin login screen
+		 */
+		public function admin_enqueue_scripts_styles() {
+            wp_register_style( 'rfg_sso_snippets_css', plugins_url( '/css/style.css', __FILE__), array(), null );
+            wp_enqueue_style( 'rfg_sso_snippets_css' );
+		}
+
+
+		/**
+		 * Outputs the actual SSO snippet
+		 */
+        public function output_snippet() {
+            ?>
+                <div class="sso-container">
+                    <?php if (!empty($_GET['saml-error'])) { ?>
+                        <p class="sso-error"><?php echo $_GET['saml-error'];?></p>
+                    <?php } ?>
+                    <a href="?option=saml_user_login&redirect_to=/wp-admin" class="button button-primary button-large sso-button">
+                        RFG Login
+                    </a>
+                </div>
+            <?php
+        }
+	}
+}
+
+new RfgSsoSnippet();
